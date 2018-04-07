6773eeeaf1a831c5f55942dc9accfe2a16ff2d87
dongsam <dongsamb@gmail.com>
2018-04-07 11:19:17 +0900

    add Mapping example

---
+++ contracts/Mapping.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.19;
+
+contract Mapping {
+    mapping(address => uint256) public myNumbers;
+    uint256 public numberCount;
+
+    function setMyNumber(uint256 _myNumber) public {
+        if (myNumbers[msg.sender] == 0) {
+            numberCount += 1;
+        }
+        myNumbers[msg.sender] = _myNumber;
+    }
+
+    function getMyNumber() public view returns(uint256 _myNumber) {
+        return myNumbers[msg.sender];
+    }
+
+    function getOtherNumber(address _address) public view returns(uint256 _myNumber) {
+        return myNumbers[_address];
+    }
+    
+    modifier checkNumber() {
+        require(myNumbers[msg.sender] != 0);
+        _;
+    }
+    
+    function deleteNumber() public checkNumber {
+        myNumbers[msg.sender] = 0;
+        numberCount -= 1;
+    }
+}
